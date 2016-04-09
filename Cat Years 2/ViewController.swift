352183147fd0488d3281f3b307eb0e2cbfaa9a01
Lawrence Chen <lawrencechen@Lawrences-Mac.local>
2016-04-08 20:41:43 -0500

    Initial commit

--- Cat Years 2/ViewController.swift
+++ Cat Years 2/ViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var catAgeTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func findAge(sender: AnyObject) {
+        
+        
+        // ! on the end of the value to unwraps the variable
+        // We are sure the variable has a value and another one on the very end
+        
+        var catAge = Int(catAgeTextField.text!)!
+        
+        catAge = catAge * 7
+        
+        resultLabel.text = "Cat Age: \(catAge)"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning()				
         // Dispose of any resources that can be recreated.
     }
 
